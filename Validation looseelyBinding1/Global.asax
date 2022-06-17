c99983ed1cc36e06a493dd822e1eaccf44d5d4d1
GAURAV PATHAK <pathakgaurav1999@gmail.com>
2022-06-17 11:50:51 +0530

    Add project files.

---
+++ Validation looseelyBinding1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Validation_looseelyBinding1.MvcApplication" Language="C#" %>
---
+++ Validation looseelyBinding1/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace Validation_looseelyBinding1.Models
+{
+    public class Employee
+    {
+        [Required(ErrorMessage ="Please enter correct firstName.")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage ="Please enter correct lastName")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage ="Please enter correct email address")]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required(ErrorMessage ="Please enter correct age range between 18 to 75")]
+        [Range(18,75)]
+        public int Age { get; set; }
+    }
+}
